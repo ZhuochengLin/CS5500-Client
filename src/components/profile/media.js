7fb3713958e5e14f65b0d057bf6ea6de277dfcc1
kunchao322 <chao.k@northeastern.edu>
2022-04-08 22:02:50 -0400

    updated media component

--- src/components/profile/media.js
+++ src/components/profile/media.js
@@ -1,8 +1,21 @@
-const Media = () => {
+import {useEffect, useState} from "react";
+import * as service from "../../services/tuits-service";
+import Tuits from "../tuits";
+
+const MyMedia = () => {
+    const [mediaTuits, setMediaTuits] = useState([]);
+    const findMyMediaTuits = () =>
+        service.findTuitsWithMediaByUser("my")
+            .then(tuits => setMediaTuits(tuits));
+    useEffect(findMyMediaTuits, []);
     return(
-        <div>
-            <h1>Media</h1>
-        </div>
+        <>
+            <h1>Tuits with media should be displayed below</h1>
+            <Tuits tuits={mediaTuits}
+                   refreshTuits={findMyMediaTuits}/>
+        </>
+
     );
 };
-export default Media;
+
+export default MyMedia;
