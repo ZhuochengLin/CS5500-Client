c7736f566dc791e469c2d9c0c6219e2a7806bbb9
kunchao322 <77757809+kunchao322@users.noreply.github.com>
2022-04-09 18:33:56 -0400

    Update tuit-images.js

--- src/components/tuits/tuit-images.js
+++ src/components/tuits/tuit-images.js
@@ -4,13 +4,13 @@ import TuitImage from "./tuit-image";
 const TuitImages = ({tuit}) => {
     const images = tuit.image || [];
   return(
-      <>
+
           <div>
               {images && images.map((image) =>
                   <TuitImage image={image}/>
               )}
           </div>
-      </>
+
   );
 };
 export default TuitImages;
