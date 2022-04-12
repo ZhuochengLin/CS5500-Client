6e269f5e1014bc43ad4f40dfdf5489a58cf7a3d6
Zhuocheng Lin <zhuochenglin@zhuochengs-mbp.myfiosgateway.com>
2022-04-12 19:14:06 -0400

    CP-56 Revert login page change

--- src/components/profile/login.js
+++ src/components/profile/login.js
@@ -1,27 +1,18 @@
-import React, {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
 import * as errorServices from "../../services/error-services";
 import {login} from "../../redux/actions";
-import {useDispatch, useSelector} from "react-redux";
-import {isLoggedIn} from "../../redux/selectors";
+import {useDispatch} from "react-redux";
 
 export const Login = () => {
     const [loginUser, setLoginUser] = useState({});
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const loggedIn = useSelector(isLoggedIn);
     const loginButtonOnClick = () => {
         login(dispatch, loginUser)
             .then(() => navigate("/profile/mytuits"))
             .catch(e => errorServices.alertError(e));
     }
-    const init = () => {
-        if (loggedIn) {
-            navigate("/home");
-        }
-    }
-    useEffect(init, []);
     return (
         <div>
             <h1>Login</h1>
