521c6efbca0baedb55a7ac024b6bbed8a6dc68ba
JC <jc.jiechen.jane@gmail.com>
2022-04-13 18:09:57 -0400

    update likes

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,35 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as likeService from "../../services/likes-service";
+import { MY } from "../../services/utils";
 
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
+const TuitStats = ({ tuit, likeTuit = () => { } }) => {
+    const [hasLiked, setHasLike] = useState(false);
+
+    useEffect(() => {
+        const updateLike = async () => {
+            const liked = await likeService.userAlreadyLikedTuit(MY, tuit._id);
+            setHasLike(!!liked);
+        };
+        updateLike()
+    },
+        [tuit]);
+
+    const likeTuitHandler = (e, tuit) => {
+        e.stopPropagation();
+        likeTuit(tuit);
+    }
+
     return (
-      <div className="row m-0 mt-2">
-        <div className="col">
-            <div className={"row align-items-center"}>
-                <i className="col fa-regular fa-message pe-0"/>
-                {
-                    tuit.stats &&
-                    <span className={"col ps-0"}>{tuit.stats.replies}</span>
-                }
+        <div className="row m-0 mt-2">
+
+            <div className="col">
+                <div className={"row align-items-center"}>
+                    <span>
+                        <i className="col fa-regular fa-message pe-0" />
+                        {
+                            tuit.stats &&
+                            <span className={"col ps-0"}>{tuit.stats.replies}</span>
+                        }
+                    </span>
+                </div>
             </div>
-        </div>
-        <div className="col">
-            <div className={"row align-items-center"}>
-                <i className="col fa-solid fa-retweet pe-0"/>
-                {tuit.stats &&
-                    <span className="col ps-0">{tuit.stats.retuits}</span>
-                }
+
+            <div className="col">
+                <div className={"row align-items-center"}>
+                    <span>
+                        <i className="col fa-solid fa-retweet pe-0" />
+                        {tuit.stats &&
+                            <span className="col ps-0">{tuit.stats.retuits}</span>
+                        }
+                    </span>
+                </div>
             </div>
-        </div>
-        <div className="col">
-            <div className={"row align-items-center"}>
-                <i className="col fa-regular fa-heart pe-0"/>
-                <span className="col ps-0">{tuit.stats && tuit.stats.likes}</span>
+
+            <div className="col">
+                <div className={"row align-items-center"}>
+                    <span onClick={(e) => likeTuitHandler(e, tuit)}>
+                        {
+                            hasLiked &&
+                            <i className="col fa-solid fa-heart pe-0" style={{ color: 'red' }}></i>
+                        }
+                        {
+                            !hasLiked &&
+                            <i className="col fa-regular fa-heart pe-0"></i>
+                        }
+                        {tuit.stats && tuit.stats.likes}
+                    </span>
+                </div>
             </div>
-        </div>
-        <div className="col-2">
-          <i className="fa-solid fa-share-from-square"/>
-        </div>
-      </div>
+
+            <div className="col-2">
+                <i className="fa-solid fa-share-from-square" />
+            </div>
+
+        </div >
     );
 }
 export default TuitStats;
