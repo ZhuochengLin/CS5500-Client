9dd2e82d69def9e37eca7e17bb2725fe3a945280
JC <jc.jiechen.jane@gmail.com>
2022-04-14 00:00:11 -0400

    update home likes

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -27,7 +27,7 @@ const Home = () => {
         }
         findTuits();
     };
-    useEffect(() => { init(); }, findTuits(), []);
+    useEffect(() => { init(); }, [findTuits]);
     return (
         <>
             {
@@ -39,7 +39,7 @@ const Home = () => {
                         {
                             tuits &&
                             <Tuits tuits={tuits}
-                                refreshTuits={init, findTuits} />
+                                refreshTuits={init} />
                         }
                     </div>
                 </div>}
